434e955a54f94f4f0d0401356edea015fade9285
Wahala <khaelterman1981@gmail.com>
2022-01-12 14:29:04 +0100

    Add objects to item list in js

---
+++ app.js
@@ -0,0 +1,82 @@
+const universes = [
+    {
+        title: 'Warhammer Fantasy',
+        description: 'Grimdark fantasy setting with a planet roughly based on earth and 2 moons.',
+        genre: ['Grimdark', 'Fantasy'],
+        origin: 'Games Workhop',
+        image: './images/warhammerf.jpg',
+        site: 'https://warhammerfantasy.fandom.com/wiki/Warhammer_Wiki'
+    },
+    {
+        title: 'Warhammer 40k',
+        description: 'Dystopian militaristic scifi setting with fantasy races.',
+        genre: ['Scifi', 'Fantasy', 'Dystopia'],
+        origin: 'Games Workshop',
+        image: '',
+        site: 'https://warhammer40k.fandom.com/wiki/Warhammer_40k_Wiki'
+    },
+    {
+        title: 'Forgotten Realms',
+        description: 'Classic fantasy setting created for Dungeons & Dragons',
+        genre: 'Fantasy',
+        origin: 'Dungeons & Dragons',
+        image: '',
+        site: 'https://forgottenrealms.fandom.com/wiki/Main_Page'
+    },
+    {
+        title: 'Ravenloft',
+        description: 'Gothic horror fantasy setting created for Dungeons & Dragons',
+        genre: ['Grimdark', 'Fantasy', 'Horror'],
+        origin: 'Dungeons & Dragons',
+        image: '',
+        site: 'https://ravenloft.fandom.com/wiki/Ravenloft_Wiki'
+    },
+    {
+        title: 'Discworld',
+        description: 'Humoristic fantasy setting taking place on a disc-like planet.',
+        genre: 'Fantasy',
+        origin: 'Novels by Terry Pratchett',
+        image: '',
+        site: 'https://discworld.fandom.com/wiki/Main_Page'
+    },
+    {
+        title: 'Star Wars',
+        description: 'Scifi setting set in a galaxy far, far away.',
+        genre: 'Scifi',
+        origin: 'George Lucas',
+        image: '',
+        site: 'https://starwars.fandom.com/wiki/Main_Page'
+    },
+    {
+        title: 'Dune',
+        description: 'Scifi universe set in a distant future of humanity.',
+        genre: 'Scifi',
+        origin: 'Novels by Frank Herbert',
+        image: '',
+        site: 'https://dune.fandom.com/wiki/Dune_Wiki'
+    },
+    {
+        title: 'Multiverse (MTG)',
+        description: 'Scifi fantasy setting taking place in a near-infinite number of different planes.',
+        genre: ['Scifi', 'Fanatsy']
+        origin: 'Magic the Gathering',
+        image: '',
+        site: 'https://mtg.fandom.com/wiki/Main_Page'
+    },
+    {
+        title: 'Middle-earth',
+        description: 'Classical fantassy setting. The origin of a lot of fantasy tropes.',
+        genre: 'Fanatsy',
+        origin: 'Novels by J.R.R Tolkien',
+        image: '',
+        site: 'https://lotr.fandom.com/wiki/Middle-earth'
+    },
+    {
+        title: 'World of A Song of Ice and Fire',
+        description: 'Fantasy setting taking place on the fictional continents Westeros and Essos.',
+        genre: 'Fantasy',
+        origin: 'Novels by George R.R. Martin',
+        image: '',
+        site: 'https://gameofthrones.fandom.com/wiki/Game_of_Thrones_Wiki'
+    },
+]
